979fb8022aae8a14d847dababf53f8c2c267c24c
Anton Chernetsov <anton.chernetsov@gmail.com>
2023-02-28 02:59:18 +0300

    fixed f

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import React from "react";
+import TodoList from "./Todo/TaskList";
+import Context from "./context";
+import NewTaskForm from "./Todo/AddTask";
+import Footer from "./Todo/Footer";
 
 function App() {
-  return <div className="wrapper"></div>;
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: "completed", title: "Completed Task" },
+    { id: 2, completed: "editing", title: "Editing Task" },
+    { id: 3, completed: "active", title: "Active Task" },
+  ]);
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTask(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: "active",
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm onCreate={addTask} />
+        </header>
+        <section className="main">
+          {todos.length ? <TodoList todos={todos} /> : <p> List is empty</p>}
+          {<Footer todos={todos} />};
+        </section>
+      </section>
+    </Context.Provider>
+  );
 }
 
 export default App;
